416e6b621835994e15614050b928aba9bb04a33f
donishyar <ahmadshahdonishyar@gmail.com>
2023-09-29 19:59:44 +0430

    working on summary section

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Summary__Card from './Summary__Card'
+import courses from "../assets/online__courses.png";
+import instructors from '../assets/instructors.png'
+import students from '../assets/students.png'
+import hours from '../assets/hours.png'
+
+
 
 const Summary = () => {
   return (
-    <div><Summary__Card/></div>
+    <section className='summary'>
+      <Summary__Card content__name={"Online Courses"} content__icon={courses} content__info={"1K+ "} />
+      <Summary__Card content__name={"Instructors"} content__icon={instructors} content__info={"100+ "} />
+      <Summary__Card content__name={"Online Students"} content__icon={students} content__info={"20K+ "} />
+      <Summary__Card content__name={"Total of Hours"} content__icon={hours} content__info={"10K hours"} />
+    </section>
   )
 }
 
--- src/components/Summary__Card.jsx
+++ src/components/Summary__Card.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Summary__Card = () => {
+const Summary__Card = ({content__icon,content__info,content__name}) => {
   return (
-    <div>Summary__Card</div>
+    <div className='summary__card'>
+      <div className="summary__card__content">
+        <div className="summary__card__content__icon__container">
+          <img className='summary__card__content__icon__image' src={content__icon}/>
+        </div>
+        <div className="summary__card__content__txt">
+          <span className="summary__card__content__txt__info">{content__info}</span>
+          <span className="summary__card__content__txt__name">{content__name}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
