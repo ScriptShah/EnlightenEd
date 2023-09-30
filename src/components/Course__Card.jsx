d7c5d5cc59aba4829c650cc0f7fcc06bf2d2dbb4
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-09-30 16:24:00 +0430

    working on course section

--- src/components/Course__Card.jsx
+++ src/components/Course__Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+
 const Course__Card = (
   { course__image,
     instructor__image,
     instructor__name,
     instructor__position,
+    instructor__heart,
     course__info,
     course__rating__stars,
     course__rating__score,
@@ -26,7 +28,9 @@ const Course__Card = (
           <span className="course__card__instructor__name">{instructor__name}</span>
           <span className="course__card__instructor__position">{instructor__position}</span>
         </div>
-        <span className="course__card__instructor__heart__sign"></span>
+        <span className="course__card__instructor__heart__sign">
+            {instructor__heart}
+        </span>
       </div>
 
       <div className="course__card__course__info">
@@ -44,7 +48,12 @@ const Course__Card = (
 
           <span className="course__card__course__rating__score">{course__rating__score}</span>
         </div>
-        <div className="course__card__course__duration">{course__duration}</div>
+        <div className="course__card__course__duration">
+          <svg xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16" fill="none">
+            <path d="M8.33333 5.33333V8.66667M14.1667 8.83333C14.1667 12.0533 11.5533 14.6667 8.33333 14.6667C5.11333 14.6667 2.5 12.0533 2.5 8.83333C2.5 5.61333 5.11333 3 8.33333 3C11.5533 3 14.1667 5.61333 14.1667 8.83333Z" stroke="#808080" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M6.33325 1.33337H10.3333" stroke="#808080" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+          {course__duration}</div>
       </div>
 
 
