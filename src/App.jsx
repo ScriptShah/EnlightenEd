9aed70824e9e3bc964ae0f9166cf8c89fc969911
donishyar <ahmadshahdonishyar@gmail.com>
2023-10-01 22:46:40 +0430

    working on footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Summary from './components/Summary'
 import Blog from './components/Blog'
+import CopyRight from './components/CopyRight'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Instructors/>
       <FeedBack/>
       <Footer/>
+      <CopyRight/>
     </div>
   )
 }
---
+++ src/components/CopyRight.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CopyRight = () => {
+  return (
+    <span className="footer__copyright">
+    @EnlightenEd 2023. All rights reserved
+    </span>
+  )
+}
+
+export default CopyRight
