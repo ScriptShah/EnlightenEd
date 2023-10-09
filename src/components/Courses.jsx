fbd15beb2e638fb3b80f5987689cfb6ac3d39cd6
Ahmad Shah Donishyar <35393325+donishyar@users.noreply.github.com>
2023-10-09 19:27:46 +0430

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -165,8 +165,6 @@ const Courses = () => {
         <div className="course__title__content">
             <div className="course__title__container">
 
-              // this svg is the lines beside the Trending Courses title
-
               <svg xmlns="http://www.w3.org/2000/svg" width="89" height="5" viewBox="0 0 89 5" fill="none">
                 <path fillRule="evenodd" clipRule="evenodd" d="M88.4276 2.96901C70.8971 0.352555 53.0702 1.79975 35.4956 1.32865C23.8743 1.01346 12.2522 0.254265 0.639411 7.60211e-05C0.291241 -0.00670236 0.00489244 0.440658 5.67348e-05 1.00326C-0.00456872 1.56587 0.274217 2.02681 0.622177 2.03359C12.2338 2.28778 23.8546 3.04697 35.4746 3.36217C53.0177 3.83327 70.811 2.38267 88.3099 4.99574C88.6568 5.04658 88.9637 4.6365 88.9974 4.07728C89.0289 3.51807 88.7724 3.02324 88.4276 2.96901Z" fill="url(#paint0_linear_148_853)"/>
                 <defs>
